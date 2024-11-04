ab0b9149f735f2d802e226ecc882ab046679b3e7
Prakruthi-16 <prakruthiu@infinite.com>
2024-11-04 22:52:33 +0530

    Added C#

---
+++ C#/Assignment-1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Data:");
+            Console.WriteLine("Input 1st Number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input 2nd NUmber:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 == num2)
+            {
+                Console.WriteLine($"{num1} and  {num2} are equal");
+            }
+            else
+            {
+                Console.WriteLine($"{num1} and {num2} are not equal");
+            }
+            Console.WriteLine("--------------------2nd Program-------------------------");
+            Console.WriteLine("Test Data:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if (num3 > 0)
+            {
+                Console.WriteLine($"{num3} is a Positive Number");
+            }
+            else
+            {
+                Console.WriteLine($"{num3} is not Positive Number");
+            }
+            Console.WriteLine("---------------------------3rd Program-----------------------------");
+            Console.WriteLine("Input Number 1 is:");
+            double num4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Opration");
+            char operation = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Input Number 2 is:");
+            double num5 = Convert.ToDouble(Console.ReadLine());
+            double res = 0;
+            bool ValidOpertion = true;
+            switch (operation)
+            {
+                case '+':
+                    res = num4 + num5;
+                    break;
+                case '-':
+                    res = num4 - num5;
+                    break;
+                case '*':
+                    res = num4 * num5;
+                    break;
+                case '/':
+                    if (num4 != 0)
+                    {
+                        res = num4 / num5;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Divisopn by  zero is not allowed.");
+                    }
+                    ValidOpertion = false;
+                    break;
+            }
+            if (ValidOpertion)
+            {
+                Console.WriteLine($"{num4} {operation} {num5} = {res}");
+            }
+            Console.WriteLine("---------------------4th Program------------------------");
+            Console.WriteLine("Enter the number6");
+            int num6 = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= 10; i++)
+            {
+                int result = num6 * i;
+                Console.WriteLine($"{num6} * {i} = {result}");
+            }
+            Console.WriteLine("---------------------5th Program--------------------");
+            Console.WriteLine("enter the num7:");
+            int num7 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the num8:");
+            int num8 = Convert.ToInt32(Console.ReadLine());
+            int sum = num7 + num8;
+            if (num7 == num8)
+            {
+                int triple = 3 * sum;
+
+                Console.WriteLine($"{num7} ,{num8} sum is {triple}");
+            }
+            else
+            {
+                sum = num7 + num8;
+                Console.WriteLine($"{num7} ,{num8} sum is {sum}");
+
+            }
+        }
+    }
+}
---
+++ C#/Assignment-2.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nBefore Swapping: num1 = {num1}, num2 = {num2}");
+
+
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine($"After Swapping: num1 = {num1}, num2 = {num2}");
+            Console.ReadLine();
+
+            Console.WriteLine("---------------2nd Program-------------");
+            Console.Write("Enter a digit: ");
+            int number = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("{0} {0} {0} {0}", number);
+            Console.WriteLine("{0}{0}{0}{0}", number);
+            Console.WriteLine("{0} {0} {0} {0}", number);
+            Console.WriteLine("{0}{0}{0}{0}", number);
+            Console.ReadLine();
+
+            Console.WriteLine("--------------3rd Program--------------");
+
+            Console.Write("Enter a day number (1 for Monday, 2 for Tuesday, ..., 7 for Sunday): ");
+            int dayNumber = int.Parse(Console.ReadLine());
+
+
+            string dayName;
+
+            switch (dayNumber)
+            {
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                case 7:
+                    dayName = "Sunday";
+                    break;
+                default:
+                    dayName = "Invalid day number";
+                    break;
+            }
+
+
+            Console.WriteLine($"The day is: {dayName}");
+            Console.ReadLine();
+
+            Console.WriteLine("---------------4th Program-----------------------");
+            int[] numbers = { 12,13,10,36,99};
+
+            double average = numbers.Average();
+
+            int minValue = numbers.Min();
+            int maxValue = numbers.Max();
+
+
+            Console.WriteLine($"Average value of array elements: {average}");
+            Console.WriteLine($"Minimum value in the array: {minValue}");
+            Console.WriteLine($"Maximum value in the array: {maxValue}");
+            Console.ReadLine();
+
+            Console.WriteLine("--------------------------------5th Program----------------------");
+            int[] marks = new int[10];
+
+            Console.WriteLine("Enter 10 marks:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Mark {i + 1}: ");
+                marks[i] = int.Parse(Console.ReadLine());
+            }
+
+            int total = marks.Sum();
+
+            double avg = marks.Average();
+
+            int minMarks = marks.Min();
+            int maxMarks = marks.Max();
+
+            Console.WriteLine($"\nTotal marks: {total}");
+            Console.WriteLine($"Average marks: {avg}");
+            Console.WriteLine($"Minimum marks: {minMarks}");
+            Console.WriteLine($"Maximum marks: {maxMarks}");
+
+            Console.WriteLine("\nMarks in ascending order:");
+            Array.Sort(marks);
+            foreach (int mark in marks)
+            {
+                Console.Write(mark + " ");
+            }
+
+            Console.WriteLine("\n\nMarks in descending order:");
+            Array.Reverse(marks);
+            foreach (int mark in marks)
+            {
+                Console.Write(mark + " ");
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("_--------------6th Program---------------");
+
+            int[] sourceArray = { 1, 2, 3, 4, 5 };
+
+            int[] targetArray = new int[sourceArray.Length];
+
+            for (int i = 0; i < sourceArray.Length; i++)
+            {
+                targetArray[i] = sourceArray[i];
+            }
+
+            Console.WriteLine("Elements in the target array:");
+            for (int i = 0; i < targetArray.Length; i++)
+            {
+                Console.Write(targetArray[i] + " ");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
