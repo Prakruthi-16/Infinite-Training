885d51835eadefb01613f6dc076de0a8cc7f6d8a
Prakruthi-16 <prakruthiu@infinite.com>
2024-11-15 07:03:40 +0530

    Assignment-5

---
+++ C#/Assignment-5/file.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign5_file
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] stringArray = new string[] {
+         "one",
+         "two",
+         "three"
+      };
+            File.WriteAllLines("hello everyone.txt", stringArray);
+            using (StreamReader sr = new StreamReader("hello everyone.txt"))
+            {
+                string res = sr.ReadToEnd();
+                Console.WriteLine(res);
+            }
+        }
+    }
+
+    
+}
