ccf38a10775ca20703f8b9be072fa540ca63f813
Prakruthi-16 <prakruthiu@infinite.com>
2025-01-08 10:42:27 +0530

    WebAPI-Assessment

---
+++ Web API/WebAPI-Assessment/WebAPI-Assessment/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_Assessment.Models
+{
+    public class Country
+    {
+        public int ID { get; set; }
+        public string CountryName { get; set; }
+        public string Capital { get; set; }
+    }
+}
